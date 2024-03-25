1313448928fe45355ed57f210d1fc24ae8f76b1c
linaTechDev <lina.ly.2018@outlook.fr>
2024-03-25 12:32:48 -0400

    ajout get all marque service, controller

--- src/main/java/org/lina/achatvoiture/controller/MarqueController.java
+++ src/main/java/org/lina/achatvoiture/controller/MarqueController.java
@@ -1,4 +1,27 @@
 package org.lina.achatvoiture.controller;
 
+import lombok.AllArgsConstructor;
+import org.lina.achatvoiture.dto.MarqueDto;
+import org.lina.achatvoiture.service.MarqueService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/marques")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MarqueController {
+    private final MarqueService marqueService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<MarqueDto>> getAllMarques() {
+        List<MarqueDto> marqueDtos = marqueService.getAllMarques();
+        return new ResponseEntity<>(marqueDtos, HttpStatus.OK);
+    }
 }
