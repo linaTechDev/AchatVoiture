74cf502fac1cdea3992acec4e852ee38fe16b9c4
linaTechDev <lina.ly.2018@outlook.fr>
2024-03-25 18:10:13 -0400

    test controller marque get all

--- src/test/java/org/lina/achatvoiture/controllers/MarqueControllerTest.java
+++ src/test/java/org/lina/achatvoiture/controllers/MarqueControllerTest.java
@@ -1,26 +1,27 @@
 package org.lina.achatvoiture.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.lina.achatvoiture.controller.MarqueController;
 import org.lina.achatvoiture.dto.MarqueDto;
-import org.lina.achatvoiture.model.Marque;
-import org.lina.achatvoiture.repository.MarqueRepository;
 import org.lina.achatvoiture.service.MarqueService;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.verify;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @AutoConfigureMockMvc(addFilters = false)
 @WebMvcTest(MarqueController.class)
@@ -28,23 +29,41 @@ public class MarqueControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
-    private MarqueRepository marqueRepository;
-
     @MockBean
     private MarqueService marqueService;
-
-
 
     @Autowired
     private ObjectMapper objectMapper;
 
+    private List<MarqueDto> marqueDtos;
+
+    @Before
+    public void setup() {
+        marqueDtos = Arrays.asList(
+                new MarqueDto(
+                        1L,
+                        "BMW"
+                ),
+                new MarqueDto(
+                        2L,
+                        "Ford"
+                ),
+                new MarqueDto(
+                        3L,
+                        "Audi"
+                )
+        );
+    }
+
     @Test
     public void testGetAllMarques() throws Exception {
-        ArrayList<Marque> marques = new ArrayList<>();
-        when(marqueRepository.findAll()).thenReturn(marques);
-        List<MarqueDto> actualMarques = marqueService.getAllMarques();
-        assertTrue(actualMarques.isEmpty());
-        verify(marqueRepository).findAll();
+        when(marqueService.getAllMarques())
+                .thenReturn(marqueDtos);
+
+        mockMvc.perform(get("/api/marques/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].marque", is("BMW")));
     }
 }
--- src/test/java/org/lina/achatvoiture/services/MarqueServiceTest.java
+++ src/test/java/org/lina/achatvoiture/services/MarqueServiceTest.java
@@ -3,6 +3,8 @@ package org.lina.achatvoiture.services;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.lina.achatvoiture.dto.MarqueDto;
+import org.lina.achatvoiture.model.Marque;
 import org.lina.achatvoiture.repository.MarqueRepository;
 import org.lina.achatvoiture.service.MarqueService;
 import org.mockito.InjectMocks;
@@ -10,6 +12,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ContextConfiguration(classes = {MarqueService.class})
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -23,6 +32,10 @@ class MarqueServiceTest {
 
     @Test
     public void testGetAllMarques() {
-
+        ArrayList<Marque> marques = new ArrayList<>();
+        when(marqueRepository.findAll()).thenReturn(marques);
+        List<MarqueDto> actualMarques = marqueService.getAllMarques();
+        assertTrue(actualMarques.isEmpty());
+        verify(marqueRepository).findAll();
     }
 }
