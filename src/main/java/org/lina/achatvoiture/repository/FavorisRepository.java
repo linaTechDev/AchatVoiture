e9932ee7b74ce53710a1837fbd6d02e6fef88cfa
linaTechDev <lina.ly.2018@outlook.fr>
2024-04-05 15:22:14 -0400

    ajout query delete voiture favoris

--- src/main/java/org/lina/achatvoiture/repository/FavorisRepository.java
+++ src/main/java/org/lina/achatvoiture/repository/FavorisRepository.java
@@ -1,9 +1,16 @@
 package org.lina.achatvoiture.repository;
 
+import jakarta.transaction.Transactional;
 import org.lina.achatvoiture.model.Favoris;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FavorisRepository extends JpaRepository<Favoris, Long> {
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Favoris WHERE id = ?1")
+    void deleteFavorisById(long id);
 }
