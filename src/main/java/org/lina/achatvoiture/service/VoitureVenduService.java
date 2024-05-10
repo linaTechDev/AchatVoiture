5efdee654d41d47a31f977db747aa85093877a4d
linaTechDev <lina.ly.2018@outlook.fr>
2024-05-09 23:56:00 -0400

    modif design recu achat voiture

--- src/main/java/org/lina/achatvoiture/service/VoitureVenduService.java
+++ src/main/java/org/lina/achatvoiture/service/VoitureVenduService.java
@@ -24,19 +24,7 @@ public class VoitureVenduService {
         VoitureVendu pannier = voitureVenduDto.toPannier();
 
         gmailSenderNotification.sendEmail(pannier.getEmail(),
-                "Destinataire : " + pannier.getFirstName() + " " + pannier.getLastName() + "\n" +
-                        " Voiture : \n" +
-                        "   Marque / modèle : " + pannier.getVoiture().getMarque() + " " + pannier.getVoiture().getModel() + "\n" +
-                        "   Détail : " + pannier.getVoiture().getDetail() + "\n" +
-                        "   Année : " + pannier.getVoiture().getAnnee() + "\n" +
-                        "   Kilométrage : " + pannier.getVoiture().getKilometrage() + "\n" +
-                        "   Roues motrices : " + pannier.getVoiture().getRouesMotrice() + "\n" +
-                        "   Nombre de cylindres : " + pannier.getVoiture().getNbrCylindre() + "\n" +
-                        "   Catégorie / taille : " + pannier.getVoiture().getCategorieTailleVoiture() + "\n" +
-                        "   Carburant : " + pannier.getVoiture().getCarburant() + "\n" +
-                        "   Transmission : " + pannier.getVoiture().getTransmission() + "\n" +
-                        "   Nombre de vitesses : " + pannier.getVoiture().getNbrVitesseTransmission() + "\n" +
-                        "   Prix : " + pannier.getVoiture().getPrix() + " $ \n");
+                pannier);
 
         return voitureVenduRepository.save(pannier).toPannierDto();
     }
--- src/main/java/org/lina/achatvoiture/service/utils/GmailSenderNotification.java
+++ src/main/java/org/lina/achatvoiture/service/utils/GmailSenderNotification.java
@@ -1,6 +1,7 @@
 package org.lina.achatvoiture.service.utils;
 
 import jakarta.transaction.Transactional;
+import org.lina.achatvoiture.model.VoitureVendu;
 import org.springframework.stereotype.Service;
 
 import java.util.Properties;
@@ -11,7 +12,7 @@ import javax.mail.internet.*;
 public class GmailSenderNotification {
 
     @Transactional
-    public void sendEmail(String emailTo, String body) {
+    public void sendEmail(String emailTo, VoitureVendu pannier) {
         final String username = "botvoiture@gmail.com";
         final String password = "fpxl irbp uhta ajna";
 
@@ -29,12 +30,40 @@ public class GmailSenderNotification {
                 });
 
         try {
+            String htmlBody = "<!DOCTYPE html>" +
+                    "<html lang=\"en\">" +
+                    "<head>" +
+                    "<meta charset=\"UTF-8\">" +
+                    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" +
+                    "<title>Notification Email</title>" +
+                    "</head>" +
+                    "<body>" +
+                    "<h2>LUXEDRIVE : Reçu d'achat de voiture</h2>" +
+                    "<p><strong>Destinataire:</strong> " + pannier.getFirstName() + " " + pannier.getLastName() + "</p>" +
+                    "<p><strong>Voiture:</strong></p>" +
+                    "<ul>" +
+                    "<li><strong>Marque / Modèle:</strong> " + pannier.getVoiture().getMarque() + " " + pannier.getVoiture().getModel() + "</li>" +
+                    "<li><strong>Détail:</strong> " + pannier.getVoiture().getDetail() + "</li>" +
+                    "<li><strong>Année:</strong> " + pannier.getVoiture().getAnnee() + "</li>" +
+                    "<li><strong>Kilométrage:</strong> " + pannier.getVoiture().getKilometrage() + "</li>" +
+                    "<li><strong>Roues motrices:</strong> " + pannier.getVoiture().getRouesMotrice() + "</li>" +
+                    "<li><strong>Nombre de cylindres:</strong> " + pannier.getVoiture().getNbrCylindre() + "</li>" +
+                    "<li><strong>Catégorie / Taille:</strong> " + pannier.getVoiture().getCategorieTailleVoiture() + "</li>" +
+                    "<li><strong>Carburant:</strong> " + pannier.getVoiture().getCarburant() + "</li>" +
+                    "<li><strong>Transmission:</strong> " + pannier.getVoiture().getTransmission() + "</li>" +
+                    "<li><strong>Nombre de vitesses:</strong> " + pannier.getVoiture().getNbrVitesseTransmission() + "</li>" +
+                    "<li><strong>Prix:</strong> " + pannier.getVoiture().getPrix() + " $</li>" +
+                    "</ul>" +
+                    "<img src=\"" + pannier.getVoiture().getImageVoiture() + "\" alt=\"Image de voiture\">" +
+                    "</body>" +
+                    "</html>";
+
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(username));
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(emailTo));
             message.setSubject("LUXEDRIVE : Reçu d'achat de voiture");
-            message.setText(body);
+            message.setContent(htmlBody, "text/html; charset=utf-8");
 
             Transport.send(message);
 
